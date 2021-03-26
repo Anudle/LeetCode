4226d012d5878060451ceed2d59d52f20de8d731
Anu <anushrestha007@gmail.com>
2021-03-26 11:43:58 -0500

    added missing numbers

--- AMZN/ArrayAndStrings/productOfArray.js
+++ AMZN/ArrayAndStrings/productOfArray.js
@@ -3,15 +3,18 @@
  * @return {number[]}
  */
  var productExceptSelf = function(nums) {
-  let result = []
-  nums.forEach((num, index) => {
-    nums.splice(index, 1) 
-    let sum = nums.reduce((a,b) => a*b)
-    result.push(sum)
-    nums.splice(index, 0, num)
-    console.log(nums)
-  })  
-  return result
+  var output = [];
+  var leftMult = 1;
+  var rightMult = 1;
+  for (var i=nums.length - 1; i >= 0; i--) {
+      output[i] = rightMult;
+      rightMult *= nums[i];
+  }
+  for (var j=0; j < nums.length; j++) {
+      output[j] *= leftMult;
+      leftMult *= nums[j];
+  }
+  return output;
 };
 
 productExceptSelf([1,2,3,4])
